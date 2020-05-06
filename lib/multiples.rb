f581e22af9d66375601c87fcf22386800d80addf
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-05-06 13:44:59 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
-# Enter your procedural solution here!
+
+#enter your procedural solution here!
+
+def collect_multiples(limit)
+    (1...limit).to_a.select {|num|num % 3 == 0 || num % 5 == 0}
+
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
+
+# array = []   (1...1000).each do |num| 
+#     if num % 3 == 0 or num % 5 == 0
+#         array << num
+#     end
+# end
+# p array
+# array.sum
